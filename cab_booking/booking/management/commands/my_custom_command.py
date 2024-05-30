266128e24fcaa8ff6562c3813876bca4093e8025
sanyam-brudite <154217691+sanyam-brudite@users.noreply.github.com>
2024-05-30 18:42:15 +0530

    Delete cab_booking directory

--- cab_booking/booking/management/commands/my_custom_command.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-
-class Command(BaseCommand):
-    help = 'Description of your custom command'
-
-    def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('Successfully ran my_custom_command'))
