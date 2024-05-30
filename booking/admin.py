8eef375ef6ad0db859662abacb294eddfef1a4b2
sanyam-brudite <154217691+sanyam-brudite@users.noreply.github.com>
2024-05-31 00:41:35 +0530

    Delete booking directory

--- booking/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Customer, Driver, RideRequest
-
-class CustomerDisplay(admin.ModelAdmin):
-    list_display = ('username', 'email')
-
-admin.site.register(Customer,CustomerDisplay)
-admin.site.register(Driver)
-admin.site.register(RideRequest)
--- booking/management/commands/__init__.py
+++
@@ -1,2 +0,0 @@
-from .my_custom_command import Command as MyCustomCommand
-from .flask_integration import Command as flask_integration
